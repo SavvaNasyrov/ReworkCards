a48f617b3dd286fc1a3d06f1a1bce94a455ec858
Савва Насыров <vasyanasyrov06288@gmail.com>
2023-05-29 13:47:40 +0500

    # "Added Class CardsDeck, xml DefaultCards"

---
+++ ReworkCards/CardsDeck.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Xml;
+
+namespace ReworkCards
+{
+    internal class CardsDeck
+    {
+        private Card[] _cards = new Card[52];
+        private string _mixingMode;
+
+        public CardsDeck() 
+        {
+            _mixingMode = ConfigurationManager.AppSettings["mixingmode"];
+            SetDefaultCards();
+        }
+
+        private bool SetDefaultCards()
+        {
+            var Reader = XmlReader.Create("DefaultCards.xml");
+            Reader.Read();
+            Reader.Read();
+            Reader.Read();
+            Reader.Read();
+            Reader.Read();
+            Reader.Read();
+            Reader.Read();
+
+            for (int i = 0; i < 52; i++)
+            {
+                _cards[Convert.ToInt32(Reader.GetAttribute("key"))] = new Card(Reader.GetAttribute("value"));
+                Reader.Read();
+                Reader.Read();
+            }
+
+            return true;
+        }
+    }
+}
--- ReworkCards/Program.cs
+++ ReworkCards/Program.cs
@@ -10,6 +10,8 @@ namespace ReworkCards
     {
         static void Main(string[] args)
         {
+
+            var deck = new CardsDeck();
         }
     }
 }
