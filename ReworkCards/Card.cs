cf30c863765971183ad71e4c4033fd872121eefa
Савва Насыров <vasyanasyrov06288@gmail.com>
2023-05-29 13:31:00 +0500

    #

---
+++ ReworkCards/Card.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReworkCards
+{
+    internal class Card
+    {
+        string _name;
+        public string Name { get { return _name; } }
+
+        public Card() { }
+        public Card(string name) => this._name = name;
+    }
+}
