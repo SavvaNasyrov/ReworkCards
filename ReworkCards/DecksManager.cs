7347c5f2ef4c8d88e6993dfda4c02f6d1d6013c8
Савва Насыров <vasyanasyrov06288@gmail.com>
2023-05-29 14:22:55 +0500

    # deck manager implementation

---
+++ ReworkCards/DecksManager.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReworkCards
+{
+    internal class DecksManager : IDecksKeeper
+    {
+        private Dictionary<string, CardsDeck> _decks = new Dictionary<string, CardsDeck>();
+        public bool HasDecks => _decks.Count != 0;
+
+        public bool CreateCardDeck(string name)
+        {
+            try
+            {
+                _decks.Add(name, new CardsDeck());
+            }
+            catch
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public bool DeleteCardDeck(string name)
+        {
+            try
+            {
+                _decks.Remove(name);
+            }
+            catch 
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public CardsDeck GetDeck(string name)
+        {
+            return _decks[name];
+        }
+
+        public string[] GetNames()
+        {
+            return _decks.Keys.ToArray();
+        }
+
+        public bool MixDeck(string name)
+        {
+            try
+            {
+                _decks[name].Mix();
+            }
+            catch 
+            { 
+                return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ ReworkCards/IDecksKeeper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReworkCards
+{
+    internal interface IDecksKeeper
+    {
+        bool HasDecks { get; }
+        bool CreateCardDeck(string name);
+        bool DeleteCardDeck(string name);
+        string[] GetNames();
+        bool MixDeck(string name);
+        CardsDeck GetDeck(string name);
+    }
+}
