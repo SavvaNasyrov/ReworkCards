c0e3e10f46e98b9ed7d1ed9df0e2fedcffe0ab50
Савва Насыров <vasyanasyrov06288@gmail.com>
2023-05-29 14:05:25 +0500

    # Mixing debug

--- ReworkCards/CardsDeck.cs
+++ ReworkCards/CardsDeck.cs
@@ -47,5 +47,66 @@ namespace ReworkCards
                 Console.WriteLine($"{i} - {_cards[i].Name}");
             }
         }
+
+        public void Mix()
+        {
+            if (_mixingMode == "hand")
+                HandMix();
+            else
+                SimpleMix();
+        }
+
+        private void HandMix()
+        {
+            Random random = new Random();
+            for (int j = 0; j < 10; j++)
+            {
+                int starts = random.Next(_cards.Length / 2 - _cards.Length / 5, _cards.Length / 2 + _cards.Length / 5);
+
+                Card[] first = new Card[starts];
+                Card[] second = new Card[_cards.Length - starts];
+
+                int counter = 0;
+
+                for (int i = 0; i < starts; i++)
+                {
+                    first[i] = _cards[i];
+                    counter = i;
+                }
+
+                counter++;
+
+                for (int i = 0; i < second.Length; i++, counter++)
+                {
+                    second[i] = _cards[counter];
+                }
+
+                counter = 0;
+
+                for (int i = 0; i < second.Length; i++)
+                {
+                    _cards[i] = second[i];
+                    counter = i;
+                }
+
+                for (int i = 0; i < first.Length; i++, counter++)
+                {
+                    _cards[counter] = first[i];
+                }
+            }
+        }
+
+        private void SimpleMix()
+        {
+            Random rand = new Random();
+            for (int i = _cards.Length - 1; i >= 1; i--)
+            {
+                int j = rand.Next() % (i + 1);
+
+                Card tmp = _cards[j];
+                _cards[j] = _cards[i];
+                _cards[i] = tmp;
+            }
+        }
     }
 }
